e93758c32112b65f509440515fdf7265f85b1c77
boyntoni <iboynton@jwplayer.com>
2019-10-27 11:43:35 -0400

    Create repo

--- jwplatform/sandbox.js
+++
@@ -1,5 +0,0 @@
-// This is an early playground for development. Will be removed. Can be run with `yarn sandbox`.
-const JWPlatformAPI = require('./api');
-
-const jw = new JWPlatformAPI('INSERT API KEY', 'INSERT API SECRET');
-console.log(jw.videos.conversions.list({ video_key: 'INSERT VIDEO KEY' }));
