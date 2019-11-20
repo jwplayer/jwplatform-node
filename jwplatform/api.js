b2266fc80ad08e92b131a11de361dce60c345522
Ian Boynton <iboynton@jwplayer.com>
2019-11-20 09:53:00 -0500

    Initial implementation for  upload method

--- jwplatform/api.js
+++ jwplatform/api.js
@@ -1,8 +1,14 @@
 'use strict';
 
+const axios = require('axios');
 const Client = require('./client');
+const FormData = require('form-data');
+const fs = require('fs');
+const utils = require('./utils');
 const Resource = require('./resource');
 const resources = require('./resources');
+const resolve = require('path').resolve;
+const qs = require('qs');
 
 class JWPlatformAPI {
     constructor(opts = { timeout: 5000 }) {
@@ -10,6 +16,7 @@ class JWPlatformAPI {
         this._client = new Client(apiKey, apiSecret, timeout);
 
         this._loadResources();
+        this.upload = this.upload.bind(this);
     }
 
     _loadResources() {
@@ -26,6 +33,29 @@ class JWPlatformAPI {
             } else {
                 this[resource] = new Resource(_client, resource);
             }
+        });
+    }
+    upload(videoOptions, filePath) {
+        const videoData = Object.assign({}, videoOptions, {
+            upload_method: 'single',
+        });
+        return this.videos.create(videoData).then(response => {
+            const { path, protocol, address } = response.link;
+            const qsParams = Object.assign({}, response.link.query, {
+                api_format: 'json',
+            });
+            const qsParamString = qs.stringify(qsParams, {
+                charset: 'utf-8',
+                sort: utils.alphabeticalSort,
+            });
+            const uploadUrl = `${protocol}://${address}${path}?${qsParamString}`;
+            const formData = new FormData();
+            formData.append('file', fs.createReadStream(resolve(filePath)));
+            return axios.post(uploadUrl, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
         });
     }
 }
